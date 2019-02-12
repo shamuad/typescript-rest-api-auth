4ebe56c6a994ebb76a1ad001bf42b6feacc23683
ARI DINAR <dinarari@gmail.com>
2019-02-12 14:24:16 +0100

    Add POST and PUT

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,5 +1,5 @@
 // src/pages/controller.ts
-import { JsonController, Get, Param } from 'routing-controllers'
+import { JsonController, Get, Put, Post, HttpCode, Param, Body } from 'routing-controllers'
 import pagesById, { Page } from './data'
 
 type PageList = { pages: Page[] }
@@ -15,9 +15,26 @@ export default class PageController {
     }
 
     @Get('/pages')
-    allPages(): PageList {
-        // .. implement!
+    allPages(): PageList { 
+
+        return { pages : Object.keys(pagesById).map(item => pagesById[item]) }
     }
 
+    @Put('/pages/:id')
+    updatePage(
+        @Param('id') id: number,
+        @Body() body: Partial<Page>
+    ): Page {
+        console.log(`Incoming PUT body param:`, body)
+        return pagesById[id]
+    }
 
+    @Post('/pages')
+    @HttpCode(201)
+    createPage(
+        @Body() body: Page
+    ): Page {
+        console.log(`Incoming POST body param:`, body)
+        return body
+    }
 }
